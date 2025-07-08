e756d2a7ae0ecf9ddc1d2198bb4d31415839a375
sukitha27 <sukithabandara13@gmail.com>
2025-07-08 18:12:31 +0530

    maintain4

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MaintenanceProvider, useMaintenanceMode } from "./contexts/MaintenanceContext";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Maintenance from "./pages/maintenance";
+import Maintenance from "./pages/Maintenance";
 
 const queryClient = new QueryClient();
 
