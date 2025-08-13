6deebd02440cb4af0888d7be345d68a39b314769
sukitha27 <sukithabandara13@gmail.com>
2025-08-14 02:06:12 +0530

    sitemapfix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    historyApiFallback: false,
+    historyApiFallback: true,
     host: "::",
     port: 8080,
   },
